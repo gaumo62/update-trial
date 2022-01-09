02244e77f56493238a09d43d44c699538754632e
Vibhakar Mohta <vib2810@gmail.com>
2022-01-09 20:40:36 +0530

    yyolo

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -42,9 +42,9 @@ namespace WinFormsApp1
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.Size = new System.Drawing.Size(143, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "RandomNess";
+            this.label1.Text = "RandomNess Edited";
             // 
             // textBox1
             // 
